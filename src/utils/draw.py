e77499545a451b09bd8b8d4f0d025146278370c3
TalalHabib123 <talalhabib101@gmail.com>
2024-07-01 15:53:44 +0500

    Alternate Code for later PIL libraries

--- src/utils/draw.py
+++ src/utils/draw.py
@@ -40,3 +40,50 @@ def draw_word_custom(word: str, style: str) -> Image:
 
     d.text(position, word, font=fnt, fill=0)
     return img
+
+# import torch
+# from PIL import Image, ImageDraw, ImageFont
+# from torchvision import transforms as T
+
+
+# def img_to_tensor(img: Image) -> torch.Tensor:
+#     transform = T.Compose([
+#         T.ToTensor(),
+#         T.Resize((64, 192))
+#     ])
+#     return transform(img)
+
+
+# def draw_word(word: str) -> Image:
+#     text_len = len(word)
+#     font_size = 50
+#     w = max(192, int(text_len * font_size * 0.64))
+#     h = 64
+
+#     img = Image.new('RGB', (w, h), color=(255, 255, 255))
+#     fnt = ImageFont.truetype('./data/VerilySerifMono.otf', font_size)
+#     d = ImageDraw.Draw(img)
+#     bbox = d.textbbox((0, 0), word, font=fnt)
+#     text_width = bbox[2] - bbox[0]
+#     text_height = bbox[3] - bbox[1]
+#     position = ((w - text_width) / 2, (h - text_height) / 2)
+
+#     d.text(position, word, font=fnt, fill=0)
+#     return img
+
+# def draw_word_custom(word: str, style: str) -> Image:
+#     text_len = len(word)
+#     font_size = 50
+#     w = max(192, int(text_len * font_size * 0.64))
+#     h = 64
+
+#     img = Image.new('RGB', (w, h), color=(255, 255, 255))
+#     fnt = ImageFont.truetype(f'./data/{style}', font_size)
+#     d = ImageDraw.Draw(img)
+#     bbox = d.textbbox((0, 0), word, font=fnt)
+#     text_width = bbox[2] - bbox[0]
+#     text_height = bbox[3] - bbox[1]
+#     position = ((w - text_width) / 2, (h - text_height) / 2)
+
+#     d.text(position, word, font=fnt, fill=0)
+#     return img
